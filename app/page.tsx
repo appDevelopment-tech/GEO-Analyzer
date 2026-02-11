3a011f7736b6b808a94fefeb01b2796a9215e972
ValeriiaMur <lera-muradyan@yandex.com>
2026-02-11 18:31:03 +0000

    producthunt

--- app/page.tsx
+++ app/page.tsx
@@ -66,6 +66,21 @@ export default function Home() {
             setEmail={setEmail}
           />
           <Features />
+          <div className="flex justify-center pt-4">
+            <a
+              href="https://www.producthunt.com/products/geoanalyzer?embed=true&utm_source=badge-featured&utm_medium=badge&utm_campaign=badge-geoanalyzer"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                alt='GeoAnalyzer - Find out if AI search can "see" your website | Product Hunt'
+                width="250"
+                height="54"
+                src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=1077960&theme=light&t=1770834455366"
+              />
+            </a>
+          </div>
           <Info />
         </motion.div>
         <Footer />
