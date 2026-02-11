7d9cbd5f9462bf1e75dd7dba2a892454ace2b0d2
ValeriiaMur <lera-muradyan@yandex.com>
2026-02-11 17:45:46 +0000

    change price to 19 back

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -16,7 +16,7 @@ export async function POST(req: NextRequest) {
         price_data: {
           currency: "usd",
           product_data: { name: "GEO/AEO/AI Full Report" },
-          unit_amount: 100, //450 cents = $4.50
+          unit_amount: 1950, //1950 cents = $19.50
         },
         quantity: 1,
       },
